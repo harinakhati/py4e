93a0b531962aed111b7682ac58f2ed56fe774a9b
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-03-06 18:26:47 +0545

    modified:   debug.py modified:   range.py

--- debug.py
+++ debug.py
@@ -1,5 +1,15 @@
+# before debugging
+
+#n=int(input("enter a number:"))
+#if n%2=0:
+#   print("even")
+#else:
+#   print("odd")
+
+
+# after debugging
 n=int(input("enter a number:"))
 if n%2==0:
-    print("even")
+   print("even")
 else:
-    print("odd")
+   print("odd")
--- range.py
+++ range.py
@@ -1,3 +1,17 @@
+
+#before debugging
+for number in range (1,10):
+  if number%3==0 and number%5==0:
+        print("fizzbuzz")
+  if number%3==0:
+        print("fizz")
+  if number%5==0:
+        print("buzz")
+  else:
+        print(number)
+
+
+# after debugging
 for number in range (1,10):
   if number%3==0 and number%5==0:
         print("fizzbuzz")
