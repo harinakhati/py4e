961ee5d8d2b481aa902211c942cbf421ce0ac94c
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-03-09 08:09:50 +0545

    modified:   range.py

--- range.py
+++ range.py
@@ -1,7 +1,7 @@
 
 #before debugging
 for number in range (1,10):
-  if number%3==0 and number%5==0:
+  if number%3==0 or number%5==0:
         print("fizzbuzz")
   if number%3==0:
         print("fizz")
